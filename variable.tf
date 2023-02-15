d751a7e32b8fbae99167f7ef264e5bdd9ec3de96
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-02-15 14:40:46 +0530

    variable.tf - added required variable part 1

--- variable.tf
+++ variable.tf
@@ -94,3 +94,183 @@ variable "vmSize" {
   #source = "instance_size.list_instance_size"
 }
 
+variable "adminUsername" {
+  type = string
+  default = "adminuser"
+  description = "User Name for Admin for perticular VM"
+}
+
+variable "adminPassword" {
+  type = string
+  description = "Admin password for the VM"
+  sensitive = true
+}
+
+variable "useManagedDisk" {
+  type = string
+  default = null
+  description = "managed disks are block-level storage volumes that are managed by Azure.Unmanaged disk consists of three data services: Blob storage, File storage, and Queue storage. In an unmanaged disk, you manage the storage accounts that you use to store the virtual hard disk (VHD) files that correspond to your VM disks. VHD files are stored as page blobs in Azure storage accounts."
+  nullable = true
+  validation {
+    condition = contains(["yes","no"],var.useManagedDisk)
+    error_message = "give only 'yes' or 'no'"
+  }
+}
+
+variable "disk_type" {
+  type = string
+  default = "Standard_LRS"
+  description = "disk type which would be attached with both os managed disks and data disks"
+  validation {
+    condition = contains(["Standard_LRS", "StandardSSD_LRS", "Premium_LRS"], var.disk_type)
+    error_message = "Select only from 'Standard_LRS', 'StandardSSD_LRS', 'Premium_LRS'"
+  }
+}
+
+variable "use_existingStorageAccount" {
+  type = string
+  default = null
+  nullable = true
+  description = "select yes/no to Host new virtual machine into existingStgAccount."
+  validation {
+    condition = contains(["yes", "no"],var.use_existingStorageAccount)
+    error_message = "Provide only yes/no"
+  }
+}
+
+variable "ExistingStorageAccountName" {
+  type = string
+  default = null
+  nullable = true
+  # source = "storage_account.list_storage_account"
+  description = "Select Existing Storage Account"
+}
+
+variable "storageAccountName" {
+  type = string
+  default = null
+  nullable = true
+  description = "New Storage Account Name"
+}
+
+variable "StorageAccountType" {
+  type = string
+  default = null
+  nullable = true
+  validation {
+    condition = contains([ "Premium_LRS", "Standard_GRS", "Standard_GZRS", "Standard_LRS", "Standard_ZRS"],var.StorageAccountType)
+    error_message = "Select the Correct Account Type"
+  }
+}
+
+variable "bootDignostics" {
+  type = string
+  default = null
+  nullable = true
+  description = "Boot diagnostics is a debugging feature for Azure virtual machines (VM) that allows diagnosis of VM boot failures. Boot diagnostics enables a user to observe the state of their VM as it is booting up by collecting serial log information and screenshots.Note:-Boot Diagnostics supports Standard_GRS,Standard_LRS storage account types."
+  validation {
+    condition = contains(["true","false"],var.bootDignostics)
+    error_message = "Put only True or False"
+  }
+}
+
+variable "RequireDataDisk" {
+  type = string
+  default = null
+  nullable = true
+  description = "select yes/no to create and attach data disks to new virtual machine."
+  validation {
+    condition = contains(["yes", "no"],var.RequireDataDisk)
+    error_message = "Give only 'yes' or 'no'"
+  }
+}
+
+variable "numDataDisk" {
+  type = number
+  default = null
+  nullable = true
+  description = "This parameter allows the user to select the number of disks they want"
+  validation {
+    condition = var.numDataDisk > 1 && var.numDataDisk < 64
+    error_message = "The Value should be between {1,64}"
+  }
+}
+
+variable "sizeOfDataDiskinGB" {
+  type = number
+  default = null
+  nullable = true
+  description = "Mention Size of the data disk"
+  validation {
+    condition = var.sizeOfDataDiskinGB > 16 && var.sizeOfDataDiskinGB < 4096
+    error_message = "Allowed size is between {16,4096} GB"
+  }
+}
+
+variable "use_ExistingVnet" {
+  type = string
+  default = null
+  nullable = true
+  description = "select yes/no to use existing virtual network and subnet for new virtual machine."
+  validation {
+    condition = contains(["yes", "no"],var.use_ExistingVnet)
+    error_message = "please give only 'yes' or 'no'"
+  }
+}
+
+variable "ExistingSubnetName" {
+  type = string
+  default = null
+  nullable = true
+  #source = virtual_network.list_all_subnet
+  description = "Specify the subnet within the virtual network where you would like to deploy this virtual machine. Do not select the gateway subnet in the virtual network. Also, do not select the dedicated subnet in which your managed domain is deployed."
+}
+
+variable "virtualNetworkName" {
+  type = string
+  default = null
+  nullable = true
+  description = "Name of new virtual network"
+}
+
+variable "addressPrefixes" {
+  type = string
+  default = null
+  nullable = true
+  description = "Mention virtual network addressprefixes range.Ex:-10.0.0.0/16"
+}
+
+variable "subnetName" {
+  type = string
+  default = null
+  description = "Name of the  virtualnetwork subnet."
+  nullable = true
+}
+
+variable "subnetRange" {
+  type = string
+  default = null
+  nullable = true
+  description = "Mention virtual network subnet range.Ex:-10.0.0.0/24"
+}
+
+variable "RequirePublicIp" {
+  type = string
+  nullable = true
+  description = "yes/no for assigning public IP or not"
+  validation {
+    condition = contains(["yes","no"],var.RequirePublicIp)
+    error_message = "give only 'yes' or 'no'"
+  }
+}
+
+variable "publicIpAddressName" {
+  type = string
+  nullable = true
+  description = "Name of new public IP resource"
+  default = null
+}
+
+variable "RequireNSG" {
+  
+}
